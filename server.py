d7b9dc26faf8b2b6b472c5cfb2384b07d28f4e35
Your Name <mahafatima14@gmail.com>
2021-11-17 18:51:47 +0000

    final working

--- server.py
+++ server.py
@@ -12,21 +12,27 @@ AWESOMENESS = [
     'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
     'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
     'wonderful', 'smashing', 'lovely']
+INSULT = ["You must have been born on a highway, because that's where most accidents happen.",
+"Shut up, you'll never be the man your mother is.",
+"It looks like your face caught on fire and someone tried to put it out with a fork.",
+"Your family tree is a cactus, because everyone on it is a prick."]
 
 
 @app.route('/')
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """<!doctype html><html>Hi! This is the home page. 
+    <br>
+    Click <a href="/hello"> hello</a> to get to the next page </html>"""
 
 
 @app.route('/hello')
 def say_hello():
-    """Say hello and prompt for user's name."""
+    """Say hello and prompt for user's name and compliment them"""
 
     return """
-    <!doctype html>
+    <!doctype html> 
     <html>
       <head>
         <title>Hi There!</title>
@@ -35,20 +41,44 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
+          <br>
+          Select a compliment:
+          <select name = "compliment">
+           <option value = "awesome"> Awesome</option>
+            <option value = "terrific"> Terrific </option>
+            <option value = "fantastic"> Fantastic</option>
+            <option value = "coolio"> Coolio</option>
+            <option value = "Wowza"> Wowza </option>
+            <option value = "Neato"> Neato</option>
+           
+            
           <input type="submit" value="Submit">
+        <br><br>
+        </form>
+
+        <form action="/diss">
+        
+          Select a diss:
+         <input type = "submit" value = "submit">
+          
+          
         </form>
       </body>
     </html>
     """
 
 
+
+
 @app.route('/greet')
 def greet_person():
-    """Get user by name."""
+    """Get user by name and compliment."""
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment =request.args.get("compliment")
+
+    
 
     return f"""
     <!doctype html>
@@ -62,6 +92,24 @@ def greet_person():
     </html>
     """
 
+@app.route('/diss')
+def diss_person():
+    """Diss the user."""
+
+    #player = request.args.get("person")
+    insult = choice(INSULT)
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Diss</title>
+      </head>
+      <body>
+         {insult}!
+      </body>
+    </html>
+    """
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads"
